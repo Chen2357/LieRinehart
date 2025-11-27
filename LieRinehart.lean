7910d04af914c84bbf3ed596239dfc2796efdd54
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2025-11-26 17:53:34 -0800

    Add Derivation.lean

--- LieRinehart.lean
+++ LieRinehart.lean
@@ -1,4 +1,5 @@
 import LieRinehart.Basic
+import LieRinehart.Derivation
 import LieRinehart.LinearMap
 import LieRinehart.DirectSum
 import LieRinehart.TensorProduct
---
+++ LieRinehart/Derivation.lean
@@ -0,0 +1,20 @@
+import LieRinehart.Basic
+
+variable (R A : Type*)
+variable [CommRing R] [CommRing A] [Algebra R A]
+
+instance : LieRinehartPair A (Derivation R A A) where
+  bracket x y := x y
+  add_lie := by simp
+  lie_add := by simp
+  leibniz_lie := by simp [Bracket.bracket]
+  lier_one := by simp
+  lier_mul := by intros; simp; ring
+  lier_smul := by intros; ext; simp [Bracket.bracket]; ring
+
+instance : LieRinehartRing A (Derivation R A A) where
+  smul_lier := by simp [Bracket.bracket]
+
+instance : LieRinehartAlgebra R A (Derivation R A A) where
+  smul_lie := by simp [Bracket.bracket]
+  lie_smul := by simp [Bracket.bracket]
